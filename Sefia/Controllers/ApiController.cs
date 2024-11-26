0188f7b37c8b4f71a01240307857e9bd3a565938
woorim02 <woorim5620@naver.com>
2024-11-26 16:20:10 +0900

    Update ApiController.cs

--- Sefia/Controllers/ApiController.cs
+++ Sefia/Controllers/ApiController.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Sefia.Common;
 using Sefia.Dtos;
@@ -9,23 +8,40 @@ namespace Sefia.Controllers
 {
     [Route("api")]
     [ApiController]
-    public class ApiController(AppSettingsService _appSettingsService) : ControllerBase
+    public class ApiController(AppSettingsService _appSettingsService, UserService _userService) : ControllerBase
     {
         [HttpGet("server-status")]
-        public async Task<ActionResult<ServerStatusDto>> GetServerStatus()
+        public ActionResult<ServerStatusDto> GetServerStatus()
         {
             var version = Assembly.GetExecutingAssembly()
                       .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
                       .InformationalVersion;
-            var isInitialized = _appSettingsService.Get(nameof(AppSettings.IsInitialized)) as bool?;
-
-            if (isInitialized == null)
-            {
-                throw new Exception("Setting: IsInitialized Must Bool");
-            }
-
-            var status = new ServerStatusDto("OK", version!, isInitialized.Value);
+            var isInitialized = _appSettingsService.Get<bool>(nameof(AppSettings.IsInitialized));
+            var status = new ServerStatusDto("OK", version!, isInitialized);
             return Ok(status);
         }
+
+        [HttpPost("init-server")]
+        public async Task<ActionResult> InitServer([FromBody] InitServerDto dto)
+        {
+            var isInitialized = _appSettingsService.Get<bool>(nameof(AppSettings.IsInitialized));
+
+            if (isInitialized)
+            {
+                return Conflict(ApiResponse.Error("The server is already initialized"));
+            }
+
+            await _userService.AddUserAsync(dto.Admin.Email, dto.Admin.Password, dto.Admin.Name, UserRoles.Admin);
+
+            _appSettingsService.Set(nameof(AppSettings.IsInitialized), true);
+            _appSettingsService.Set(nameof(AppSettings.ApplicationDomain), dto.ApplicationDomain);
+            _appSettingsService.Set(nameof(AppSettings.ServingDomain), dto.ServingDomain);
+            _appSettingsService.Set(nameof(AppSettings.WebRoot), dto.WebRoot);
+
+            _appSettingsService.SaveToFile();
+
+            return Ok(ApiResponse.Success());
+        }
+
     }
 }
