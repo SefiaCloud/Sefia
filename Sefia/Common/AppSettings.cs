c405336ad23709638d5b57709f72e36d7ffe0ac2
woorim02 <woorim5620@naver.com>
2024-11-26 16:19:41 +0900

    Add Domain Settings

--- Sefia/Common/AppSettings.cs
+++ Sefia/Common/AppSettings.cs
@@ -3,4 +3,8 @@ namespace Sefia.Common;
 public class AppSettings
 {
     public bool IsInitialized { get; set; } = false;
+
+    public string ApplicationDomain { get; set; } = string.Empty;
+    public string ServingDomain { get; set; } = string.Empty;
+    public string WebRoot { get; set; } = string.Empty;
 }
